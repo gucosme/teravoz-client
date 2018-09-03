f8fd2538bd096fc7c3bce55717fa468da802bd57
Gustavo Cosme <gustavocosme@outlook.com>
2018-09-03 11:11:24 -0300

    Checking if body is valid

--- src/components/webhook.js
+++ src/components/webhook.js
@@ -1,21 +1,22 @@
 const createError = require('http-errors')
-const debug = require('debug')('service:controllers:webhook')
+const debug = require('debug')('service:components:webhook')
 
 module.exports = app => {
   const { call, actor } = app.schemas
 
   const index = async (req, res, next) => {
     debug('index')
+    const { body } = req
 
-    const callValid = await call.isValid()
-    const actorValid = await actor.isValid()
+    const callValid = await call.isValid(body)
+    const actorValid = await actor.isValid(body)
 
     if (!(callValid || actorValid)) {
-      const err = createError(400, 'Missing body')
+      const err = createError(400, 'Body is not a valid object')
       return next(err)
     }
 
-    res.status(200).json({ some: 'shit' })
+    res.status(200).json({ status: 'ok' })
     next()
   }
 
