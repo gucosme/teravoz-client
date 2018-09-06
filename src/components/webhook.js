20caadadc775319433f954f73fce2b0788ff56fc
Gustavo Cosme <gustavocosme@outlook.com>
2018-09-06 10:58:50 -0300

    Handling call types and customers

--- src/components/webhook.js
+++ src/components/webhook.js
@@ -1,11 +1,33 @@
+const R = require('ramda')
 const createError = require('http-errors')
 const debug = require('debug')('service:components:webhook')
 
 module.exports = app => {
+  const { teravoz } = app.components
   const { call, actor } = app.schemas
+  const { callStore, customerStore } = app.stores
+  const OK = { status: 'ok' }
+
+  const setOrUpdateCall = async call => {
+    if (R.equals(call.type, 'call.new')) await callStore.set(call)
+    else await callStore.update({ call_id: call.call_id }, call)
+  }
+
+  const checkAndRegister = async incomingCustomer => {
+    const customers = await customerStore.get()
+    const customer = R.find(R.equals(incomingCustomer), customers)
+    if (!customer) {
+      await customerStore.set(incomingCustomer)
+      return true
+    }
+    return false
+  }
 
   const index = async (req, res, next) => {
     debug('index')
+    await callStore.init()
+    await customerStore.init()
+
     const { body } = req
 
     const callValid = await call.isValid(body)
@@ -16,7 +38,19 @@ module.exports = app => {
       return next(err)
     }
 
-    res.status(200).json({ status: 'ok' })
+    if (actorValid) {
+      res.send(200).json(OK)
+      next()
+    }
+
+    await setOrUpdateCall(body)
+
+    if (body.type === 'call.standby') {
+      const isNew = await checkAndRegister(R.pick(['their_number'], body))
+      await teravoz.delegate(body, isNew ? 900 : 901)
+    }
+
+    res.status(200).json(OK)
     next()
   }
 
