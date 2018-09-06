7818c109ef4618da93c0fe0f0b7baded9ac4abd4
Gustavo Cosme <gustavocosme@outlook.com>
2018-09-06 11:19:30 -0300

    Improved test to assert calls to teravoz API

--- tests/webhook.spec.js
+++ tests/webhook.spec.js
@@ -1,6 +1,7 @@
 const R = require('ramda')
 const { expect } = require('chai')
 const supertest = require('supertest')
+const sinon = require('sinon')
 
 const call = require('./fixtures/call.json')
 const error = require('./fixtures/error.json')
@@ -10,15 +11,21 @@ const request = supertest(app)
 const callStore = app.stores.callStore
 const customerStore = app.stores.customerStore
 
+const { teravoz } = app.components
+
 describe('POST /webhook', () => {
   before(async () => {
     await callStore.init()
     await customerStore.init()
+
+    sinon.replace(teravoz, 'delegate', sinon.fake())
   })
 
   after(async () => {
     await callStore.clear()
     await customerStore.clear()
+
+    sinon.restore()
   })
 
   it('should respond with error, missing body', async () => {
@@ -48,9 +55,14 @@ describe('POST /webhook', () => {
 
     const calls = await callStore.get()
     const customers = await customerStore.get()
+    const { delegate } = app.components.teravoz
+
     expect(calls.length).to.be.equal(1)
     expect(calls[0]).to.be.deep.equal(call.standby)
     expect(customers.length).to.be.equal(1)
     expect(customers[0]).to.be.deep.equal(R.pick(['their_number'], call.standby))
+
+    expect(delegate.callCount).to.be.equal(1)
+    expect(delegate.getCall(0).args).to.be.deep.equal([call.standby, 900])
   })
 })
