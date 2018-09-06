0b08b9ad5d48de80326db87eb102ee8393ba193c
Gustavo Cosme <gustavocosme@outlook.com>
2018-09-06 10:57:23 -0300

    Component to integrate with teravoz api

--- src/components/teravoz.js
+++ src/components/teravoz.js
@@ -1,14 +1,31 @@
+/**
+ * This module is just for demonstration and test purposes
+ * It will not interact with teravoz API and will always
+ * return { status: 'ok' }
+ */
 const axios = require('axios')
 const debug = require('debug', 'service:components:teravoz')
 
 module.exports = app => {
-  const BASE_URL = 'https://gusta.free.beeceptor.com/api'
+  const BASE_URL = 'https://api.teravoz.com'
 
   const delegate = async call => {
     debug('delegate')
+    const opts = {
+      method: 'POST',
+      url: `${BASE_URL}/actions`,
+      data: call,
+      json: true,
+      headers: {
+        'Authorization': 'Basic dXNlcjpwYXNzdw=='
+      }
+    }
+
+    // Forcing return to avoid unwanted fetchs
+    if (opts) return { status: 'ok' }
 
     try {
-      const { data } = await axios.post(`${BASE_URL}/actions`, call)
+      const { data } = await axios(opts)
       return data
     } catch (error) {
       return error
