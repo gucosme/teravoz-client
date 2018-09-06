b1de6e9a0434ddfaa9d9855bec409421e71bd47f
Gustavo Cosme <gustavocosme@outlook.com>
2018-09-06 11:00:30 -0300

    Added new tests and improved the ones that already existed

--- tests/webhook.spec.js
+++ tests/webhook.spec.js
@@ -1,50 +1,56 @@
+const R = require('ramda')
 const { expect } = require('chai')
 const supertest = require('supertest')
 
+const call = require('./fixtures/call.json')
+const error = require('./fixtures/error.json')
+
 const app = require('../src/server.js')
 const request = supertest(app)
-const callStore = app.components.store('call-test')
+const callStore = app.stores.callStore
+const customerStore = app.stores.customerStore
 
 describe('POST /webhook', () => {
-  const badRequestError = {
-    error: {
-      title: 'Bad Request',
-      name: 'BadRequestError',
-      message: 'Body is not a valid object'
-    }
-  }
-
-  const call = {
-    type: 'call.new',
-    call_id: '1463669263.30033',
-    code: '123456',
-    direction: 'inbound',
-    our_number: '0800000000',
-    their_number: '11999990000',
-    their_number_type: 'mobile',
-    timestamp: '2018-09-01T00:00:00Z'
-  }
-
   before(async () => {
     await callStore.init()
+    await customerStore.init()
   })
 
   after(async () => {
     await callStore.clear()
+    await customerStore.clear()
   })
 
   it('should respond with error, missing body', async () => {
     const { status, body } = await request.post('/webhook')
     expect(status).to.be.equal(400)
-    expect(body).to.be.deep.equal(badRequestError)
+    expect(body).to.be.deep.equal(error.badRequestError)
   })
 
-  it('should save a new call and register the number from the new user', async () => {
+  it('should save a new call', async () => {
     const { status, body } =
       await request.post('/webhook')
-        .send(call)
+        .send(call.new)
     expect(status).to.be.equal(200)
     expect(body).to.be.deep.equal({ status: 'ok' })
-    expect(callStore.get('calls').lenght).to.be.equal(1)
+
+    const calls = await callStore.get()
+    expect(calls.length).to.be.equal(1)
+    expect(calls[0]).to.be.deep.equal(call.new)
+  })
+
+  it('should update call type, add a new customer and then delegate the call', async () => {
+    const { status, body } =
+      await request.post('/webhook')
+        .send(call.standby)
+    expect(status).to.be.equal(200)
+    expect(body).to.be.deep.equal({ status: 'ok' })
+
+    const calls = await callStore.get()
+    const customers = await customerStore.get()
+    expect(calls.length).to.be.equal(1)
+    expect(calls[0]).to.be.deep.equal(call.standby)
+    expect(customers.length).to.be.equal(1)
+    expect(customers[0]).to.be.deep.equal(R.pick(['their_number'], call.standby))
   })
 })
